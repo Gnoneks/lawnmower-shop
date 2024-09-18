6740c3ec42a80ab4b96150e15f8f336fa8b96a34
Marcin Żuk <Gnoneks@gmail.com>
2024-09-18 17:46:41 +0200

    003--Start creating order form

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule, MatRadioModule],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.scss'
+  styleUrl: './order.component.scss',
+  providers: [OrderService],
 })
 export class OrderComponent {
+  orderForm = this._orderService.initOrderForm();
 
+  constructor(private readonly _orderService: OrderService) {}
 }
