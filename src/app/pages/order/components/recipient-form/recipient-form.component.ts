1279106de0e1e553c5b5145eba1dee22d73ae9e3
Marcin Żuk <Gnoneks@gmail.com>
2024-09-19 16:08:03 +0200

    003--Add error messages to fields

--- src/app/pages/order/components/recipient-form/recipient-form.component.ts
+++ src/app/pages/order/components/recipient-form/recipient-form.component.ts
@@ -4,14 +4,22 @@ import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
 import { RecipientForm } from '../../models/order-form.model';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { RECIPIENT_FORM } from './recipient-form.data';
 
 @Component({
   selector: 'app-recipient-form',
   standalone: true,
-  imports: [ReactiveFormsModule, InputMaskModule, InputTextModule, FloatLabelModule],
+  imports: [
+    ReactiveFormsModule,
+    InputMaskModule,
+    InputTextModule,
+    FloatLabelModule,
+  ],
   templateUrl: './recipient-form.component.html',
   styleUrl: './recipient-form.component.scss',
 })
 export class RecipientFormComponent {
   recipientForm = input.required<FormGroup<RecipientForm>>();
+
+  readonly recipientFormData = RECIPIENT_FORM;
 }
